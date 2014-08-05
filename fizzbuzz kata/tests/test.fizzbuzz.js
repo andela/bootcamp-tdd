f9763624868c9d7ad481a14b1705ed3a0b3ff86d
nenegesi <nenegesi@gmail.com>
2014-08-05 19:25:41 +0100

    update tests

--- fizzbuzz kata/tests/test.fizzbuzz.js
+++ fizzbuzz kata/tests/test.fizzbuzz.js
@@ -16,12 +16,5 @@ describe("FizzBuzz", function(){
         sut.run(5).should.equal("Buzz");
     });
 
-    it("should return number for number not divisible by 3 or 5 => 7", function(){
-        sut.run(7).should.equal(7);
-    });
-
-    it("should return FizzBuzz for number divisible by 15", function(){
-        sut.run(30).should.equal("FizzBuzz");
-    });
 
 });
